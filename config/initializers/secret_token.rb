4486009c25529c69069482207d28639f70a86064
Luke Imhoff <Kronic.Deth@gmail.com>
2013-01-26 15:44:20 -0600

    rails new underdork
    --database postgresql
    --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Underdork::Application.config.secret_token = 'a62abd0bb1dec14fc95a8be5d5c4a0fb1c5736669754c4de0c7c238ddeacc7576997641dd824e7260f703a382caa1640001d56052f024f12d949701b73416a3b'
