fd87147a921459c996cad1dde8e1bb8c4aa15382
Luke Imhoff <Kronic.Deth@gmail.com>
2013-01-26 17:20:46 -0600

    Generate secret token as needed
    Don't store the secret token in config/initializers/secret_token.rb, but
    in config/secret_token.  config/secret_token is gitignored and generated
    on-the-fly if it does not exist.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,35 @@
 # Be sure to restart your server when you modify this file.
 
+retried = false
+secret_token = nil
+# Store the secret token in a file that is not committed to git
+secret_token_pathname = Rails.root.join('config', 'secret_token')
+
+begin
+  secret_token_pathname.open('r') do |f|
+    secret_token = f.read
+  end
+# generate a secret token and write it to the file if the file does not exist
+rescue Errno::ENOENT
+  if retried
+    raise
+  end
+
+  # same thing `rake secret` does
+  secret_token = SecureRandom.hex(64)
+
+  secret_token_pathname.open('w') do |f|
+    f.write(secret_token)
+  end
+
+  # set flag so this doesn't get stuck in a loop
+  retried = true
+  # retry to ensure the file is readable
+  retry
+end
+
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Underdork::Application.config.secret_token = 'a62abd0bb1dec14fc95a8be5d5c4a0fb1c5736669754c4de0c7c238ddeacc7576997641dd824e7260f703a382caa1640001d56052f024f12d949701b73416a3b'
+Underdork::Application.config.secret_token = secret_token
